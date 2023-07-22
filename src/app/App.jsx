9ddb9960241ba9facb63f1f6b386d7f3492da97d
grimoll2e <itmutcpe6@gmail.com>
2023-07-22 17:54:44 +0700

    spinner

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,14 +2,16 @@ import "./App.scss";
 // import TodoPage from "../pages/TodoPage";
 // import LoginPage from '../pages/LoginPage';
 // import RegisterPage from '../pages/RegisterPage'
-import ProfilePage from '../pages/ProfilePage'
+// import ProfilePage from '../pages/ProfilePage'
+import {Spinner} from '../components/Common/Spinner'
 
 function App() {
 
   // return <TodoPage/>;
   // return <RegisterPage/>;
   // return <LoginPage/>;
-  return <ProfilePage/>
+  // return <ProfilePage/>
+  return <Spinner />
 }
 
 export default App;
---
+++ src/components/Common/Spinner.jsx
@@ -0,0 +1,17 @@
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+import { Translate } from '@mui/icons-material';
+
+export function Spinner() {
+    return (
+        <div className='relative h-screen'>
+            <Box sx={{ display: 'flex', 
+            position: 'absolute', 
+            top: '50%', 
+            left: '50%', 
+            transform: `translate(-${50}%,-${50}%)` }}>
+                <CircularProgress />
+            </Box>
+        </div>
+    );
+}
